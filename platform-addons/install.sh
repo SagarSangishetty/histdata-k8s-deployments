80d1a748e91d4c21637148dfcfdc0465f198ec5d
SagarSangishetty <shettysagar272@gmail.com>
2026-09-17 06:25:01 +0530

    fixed install.sh issue releated ALBB and EOS

--- platform-addons/install.sh
+++ platform-addons/install.sh
@@ -22,14 +22,14 @@ helm upgrade --install aws-load-balancer-controller eks/aws-load-balancer-contro
   --set vpcId="$VPC_ID" \
   --set serviceAccount.create=true \
   --set serviceAccount.name=aws-load-balancer-controller \
-  --set-string serviceAccount.annotations.eks\.amazonaws\.com/role-arn="$ALB_CONTROLLER_ROLE_ARN"
+  --set-string "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn=${ALB_CONTROLLER_ROLE_ARN}"
 
 helm upgrade --install external-secrets external-secrets/external-secrets \
   --namespace external-secrets \
   --set installCRDs=true \
   --set serviceAccount.create=true \
   --set serviceAccount.name=external-secrets \
-  --set-string serviceAccount.annotations.eks\.amazonaws\.com/role-arn="$EXTERNAL_SECRETS_ROLE_ARN"
+  --set-string "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn=${EXTERNAL_SECRETS_ROLE_ARN}"
 
 kubectl rollout status deployment/aws-load-balancer-controller -n kube-system --timeout=180s
 kubectl rollout status deployment/external-secrets -n external-secrets --timeout=180s
